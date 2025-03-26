4d849a18a062e4bce89c0cd2a5bba7654a631c0f
juangomcar <jf.gomez4224@gmail.com>
2025-03-25 23:26:51 -0500

    Resolver conflicto y nuevo deploy final

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Esquema y modelo
 const PacienteSchema = new mongoose.Schema({
   id: Number,
   nombre: String,
   estado: String
 });
 
-// El nombre exacto de la colección es 'Paciente' y la base de datos también
 const Paciente = mongoose.model('Paciente', PacienteSchema, 'Paciente');
 
-// Conexión a la base de datos correcta (Paciente)
-mongoose.connect('mongodb://jfgomez4224:6iLNFn2UhbuOpktD@ac-ccq6eni-shard-00-00.jnt0kh7.mongodb.net/Paciente?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://jfgomez4224:6iLNFn2UhbuOpktD@ac-ccq6eni-shard-00-00.jnt0kh7.mongodb.net/Paciente?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => {
   console.log('✅ Conectado a MongoDB Atlas');
+
   app.listen(port, () => {
     console.log(`Servidor corriendo en puerto ${port}`);
   });
@@ -28,12 +26,10 @@ mongoose.connect('mongodb://jfgomez4224:6iLNFn2UhbuOpktD@ac-ccq6eni-shard-00-00.
   console.error('❌ Error al conectar a MongoDB', err);
 });
 
-// Endpoint raíz
 app.get('/', (req, res) => {
   res.send('Sistema HUSI funcionando');
 });
 
-// Endpoint real desde MongoDB
 app.get('/pacientes', async (req, res) => {
   try {
     const pacientes = await Paciente.find();
