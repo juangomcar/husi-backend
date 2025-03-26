f31e9e9396143422f24ff2450cc872852ddba41f
Juan Gomez <140413447+juangomcar@users.noreply.github.com>
2025-03-25 22:17:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,25 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Conexión a MongoDB Atlas con base de datos explícita
-mongoose.connect('mongodb://jfgomez4224:6iLNFn2UhbuOpktD@ac-ccq6eni-shard-00-00.jnt0kh7.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://jfgomez4224:6iLNFn2UhbuOpktD@ac-ccq6eni-shard-00-00.jnt0kh7.mongodb.net/husi?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('✅ Conectado a MongoDB Atlas'))
 .catch(err => console.error('❌ Error al conectar a MongoDB', err));
 
-// Esquema y modelo
 const PacienteSchema = new mongoose.Schema({
   id: Number,
   nombre: String,
   estado: String
 });
 
-const Paciente = mongoose.model('pacientes', PacienteSchema); // <- mismo nombre que en Atlas
+const Paciente = mongoose.model('Paciente', PacienteSchema, 'pacientes'); // <-
 
-// Endpoint raíz
 app.get('/', (req, res) => {
   res.send('Sistema HUSI funcionando');
 });
 
-// Endpoint real desde MongoDB
 app.get('/pacientes', async (req, res) => {
   try {
     const pacientes = await Paciente.find();
